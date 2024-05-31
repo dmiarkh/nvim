d2bb770f4cce4c83d38641e0be8003970cdcfb7d
DmytroArkhypov <dmiarkh@gmail.com>
2024-05-31 16:26:46 +0300

    added harpoon

---
+++ lua/config/plugins/harpoon.lua
@@ -0,0 +1,32 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local harpoon = require("harpoon")
+
+		-- REQUIRED
+		harpoon:setup()
+		-- REQUIRED
+
+		vim.keymap.set("n", "<leader>ha", function()
+			harpoon:list():add()
+		end)
+		vim.keymap.set("n", "<leader>hl", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
+
+		vim.keymap.set("n", "<A-a>", function()
+			harpoon:list():select(1)
+		end)
+		vim.keymap.set("n", "<A-s>", function()
+			harpoon:list():select(2)
+		end)
+		vim.keymap.set("n", "<A-d>", function()
+			harpoon:list():select(3)
+		end)
+		vim.keymap.set("n", "<A-f>", function()
+			harpoon:list():select(4)
+		end)
+	end,
+}
