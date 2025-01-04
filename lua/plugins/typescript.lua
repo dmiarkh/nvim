ac71543ba41211aded4e6ebacf3a5863462f2738
DmytroArkhypov <dmiarkh@gmail.com>
2025-01-04 16:40:53 +0200

    load ts-tools only on related ft

--- lua/plugins/typescript.lua
+++ lua/plugins/typescript.lua
@@ -1,5 +1,6 @@
 return {
     "pmizio/typescript-tools.nvim",
+    ft = { "typescript", "typescriptreact", "javascriptreact", "javascript" },
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
     opts = function()
         vim.keymap.set("n", "<leader>co", "<cmd>TSToolsOrganizeImports<cr>", { desc = "Ortanize ts imports" })
