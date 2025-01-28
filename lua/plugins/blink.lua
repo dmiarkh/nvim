7356e87ffcb879274c6a72a8a9eea4a6bd714761
DmytroArkhypov <dmiarkh@gmail.com>
2025-01-28 20:44:27 +0200

    add jsregexp for luasnip

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -5,6 +5,7 @@ return {
         "L3MON4D3/LuaSnip",
         version = "v2.*",
         dependencies = { "rafamadriz/friendly-snippets" },
+        build = "make install_jsregexp",
         config = function()
             require("luasnip.loaders.from_vscode").lazy_load()
         end,
