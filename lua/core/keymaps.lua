1c77cb1b46ed1c9924d549619ad16513af4f2498
DmytroArkhypov <dmiarkh@gmail.com>
2025-04-07 23:37:30 +0300

    add bufdelete notification

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -6,6 +6,7 @@ vim.keymap.set({ "n", "x" }, "k", "v:count ? 'k' : 'gk'", { desc = "Up", expr = 
 -- vim.keymap.set("n", "<leader>bx", "<cmd>%bd|e#|bd#<cr>", { desc = "Close all buffers but the current one" })
 vim.keymap.set("n", "<leader>bx", function()
 	Snacks.bufdelete.other()
+	vim.notify("Deleted all buffers")
 end, { desc = "Close all buffers but the current one" })
 vim.keymap.set("n", "<leader>ql", "<cmd>copen<cr>", { desc = "Open quickfix" })
 
