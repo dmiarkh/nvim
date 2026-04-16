3f62e68523638478c6379bfee2f0ea650b63b9d4
Dmytro Arkhypov <dmiarkh@gmail.com>
2026-04-16 23:41:45 +0300

    add diffget keybinds

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,3 +1,5 @@
 vim.pack.add({ "https://github.com/tpope/vim-fugitive" })
 
 vim.keymap.set("n", "<leader>G", "<cmd>Git<cr>", { desc = "Fugitive" })
+vim.keymap.set("n", "<leader>2", "<cmd>diffget //2<cr>", { desc = "Get from HEAD" })
+vim.keymap.set("n", "<leader>3", "<cmd>diffget //3<cr>", { desc = "Get from target" })
